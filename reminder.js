bf415560a0a56255d2acd3f732f2d17854c29a19
Victor Tyavbee <68505976+Iamtsquare07@users.noreply.github.com>
2023-11-22 03:05:49 +0100

    Add files via upload

--- reminder.js
+++ reminder.js
@@ -74,3 +74,63 @@ document.addEventListener("DOMContentLoaded", function () {
     reminderList.appendChild(reminderItem);
   });
 });
+
+// Add this function to your script
+function setReminder(defaultTime) {
+  const timePicker = document.getElementById("timePicker");
+  const reminderList = document.getElementById("reminderList");
+
+  let reminderTime;
+
+  if (document.getElementById("switch-time").checked) {
+    // Use the selected time from the time picker
+    reminderTime = timePicker.value;
+  } else {
+    // Use the default time suggestion
+    reminderTime = defaultTime;
+  }
+
+  const reminderInput = document.getElementById("reminderInput");
+
+  if (reminderInput.value.trim() === "") {
+    alert("Please enter a reminder.");
+    return;
+  }
+
+  const reminderText = reminderInput.value;
+  const reminderItem = document.createElement("li");
+  reminderItem.className = "reminderItem";
+
+  const deleteBtn = document.createElement("span");
+  deleteBtn.className = "deleteBtn";
+  deleteBtn.innerText = "Delete";
+  deleteBtn.onclick = function () {
+    deleteReminder(reminderItem);
+  };
+
+  reminderItem.innerHTML = `${reminderText} - ${reminderTime}`;
+  reminderItem.appendChild(deleteBtn);
+
+  reminderList.appendChild(reminderItem);
+
+  // Save to localStorage
+  saveToLocalStorage(`${reminderText} - ${reminderTime}`);
+
+  // Clear input field
+  reminderInput.value = "";
+
+  // Hide the time picker container
+  document.getElementById("timePickerContainer").style.display = "none";
+}
+
+// Modify the existing function to toggle the time picker container
+function toggleTimePicker() {
+  const timePickerContainer = document.getElementById("timePickerContainer");
+  const switchTime = document.getElementById("switch-time");
+
+  if (switchTime.checked) {
+    timePickerContainer.style.display = "block";
+  } else {
+    timePickerContainer.style.display = "none";
+  }
+}
